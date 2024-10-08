2f575fae3f199ba10431b151435cb16dfdc8d7d7
theanimated01 <79371675+theanimated01@users.noreply.github.com>
2024-10-07 22:59:12 -0700

    hello-world app with animation

---
+++ hello-world/hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Animesh on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
